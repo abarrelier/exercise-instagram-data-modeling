d5917e3b0e9774936a1aada32f8354fd4e0c211d
abarrelier <128666422+abarrelier@users.noreply.github.com>
2023-08-09 06:04:16 +0000

    v01

--- src/models.py
+++ src/models.py
@@ -4,26 +4,80 @@ from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    username = Column(String(50), unique=True, nullable=False)
+    email = Column(String(100), unique=True, nullable=False)
+    password = Column(String(100), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+    posts = relationship('Post', back_populates='user') # back indica el lado inverso de una relación bidireccional entre dos modelos.
+    followers = relationship('Follow', foreign_keys='Follow.following_id', back_populates='following')#indicar clave foranea
+    following = relationship('Follow', foreign_keys='Follow.follower_id', back_populates='follower')
+    likes = relationship('Like', back_populates='user')
+    comments = relationship('Comment', back_populates='user')
+    messages_sent = relationship('Message', foreign_keys='Message.sender_id', back_populates='sender')
+    messages_received = relationship('Message', foreign_keys='Message.receiver_id', back_populates='receiver')
+
+class Post(Base):
+    __tablename__ = 'post'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    image_url = Column(String(200), nullable=False)
+    caption = Column(String(200))
+    created_at = Column(DateTime, nullable=False)
+
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship('User', back_populates='posts')
+    likes = relationship('Like', back_populates='post')
+    comments = relationship('Comment', back_populates='post')
+
+class Like(Base):
+    __tablename__ = 'like'
+    id = Column(Integer, primary_key=True)
+
+    user_id = Column(Integer, ForeignKey('user.id'))
+    post_id = Column(Integer, ForeignKey('post.id'))
+    user = relationship('User', back_populates='likes')
+    post = relationship('Post', back_populates='likes')
+
+class Comment(Base):
+    __tablename__ = 'comment'
+    id = Column(Integer, primary_key=True)
+    text = Column(String(200), nullable=False)
+
+    user_id = Column(Integer, ForeignKey('user.id'))
+    post_id = Column(Integer, ForeignKey('post.id'))
+    user = relationship('User', back_populates='comments')
+    post = relationship('Post', back_populates='comments')
+
+class Follow(Base):
+    __tablename__ = 'follow'
+    id = Column(Integer, primary_key=True)
+
+    follower_id = Column(Integer, ForeignKey('user.id'))
+    following_id = Column(Integer, ForeignKey('user.id'))
+    follower = relationship('User', foreign_keys=[follower_id], back_populates='following')
+    following = relationship('User', foreign_keys=[following_id], back_populates='followers')
+
+class Message(Base):
+    __tablename__ = 'message'
+    id = Column(Integer, primary_key=True)
+    text = Column(String(200), nullable=False)
+    sent_at = Column(DateTime, nullable=False)
+
+    sender_id = Column(Integer, ForeignKey('user.id'))
+    receiver_id = Column(Integer, ForeignKey('user.id'))
+    sender = relationship('User', foreign_keys=[sender_id], back_populates='messages_sent')
+    receiver = relationship('User', foreign_keys=[receiver_id], back_populates='messages_received')
+
+
+
 
     def to_dict(self):
         return {}
